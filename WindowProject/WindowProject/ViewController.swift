8dbc0a0b8d0278e8cfc77f5b348cc1dcebbe7f1b
Ram Prakash Singh <rpsinghc@gmail.com>
2019-06-22 19:06:10 +0530

    code commit

---
+++ WindowProject/WindowProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WindowProject
+//
+//  Created by Ram Prakash Singh on 18/01/19.
+//  Copyright © 2019 Ram Prakash Singh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ helloworld.playground/Contents.swift
@@ -0,0 +1,61 @@
+//
+//  main.swift
+//  PhoneUtility
+//
+//  Created by Ram Prakash Singh on 18/01/19.
+//  Copyright © 2019 Ram Prakash Singh. All rights reserved.
+//
+
+import Foundation
+import IOBluetooth
+import CoreFoundation
+import Cocoa
+import PlaygroundSupport
+
+
+
+class BlueDelegate : IOBluetoothDeviceInquiryDelegate {
+    func deviceInquiryComplete(sender: IOBluetoothDeviceInquiry, error: IOReturn, aborted: Bool) {
+        aborted
+        let devices = sender.foundDevices()
+        for device : Any in devices! {
+            print(device)
+            if let thingy = device as? IOBluetoothDevice {
+                thingy.getAddress()
+            }
+        }
+        print(devices ??  nil)
+    }
+}
+
+func main() {
+    let str:NSString = NSString(string: "Hello World");
+    print(str.appending(", Raa"))
+    
+    let st = "Hello Wrld"
+    print(st + ", Raa")
+    let str1 = "-"+str.substring(from: 0)
+    print(str1)
+    print(NSUUID().uuidString)
+    print(NSUserName())
+    
+    print(getppid())
+    
+    print(NSFullUserName())
+    let inquiry = IOBluetoothDeviceInquiry(delegate: BlueDelegate())!
+    inquiry.start()
+    print("start")
+    DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(60), execute: {
+        // Put your code which should be executed with a delay here
+        print("stop")
+    })
+    
+    
+    print(inquiry.foundDevices())
+    //XCPSetExecutionShouldContinueIndefinitely()
+    PlaygroundPage.current.needsIndefiniteExecution = true
+}
+
+main()
+
+
